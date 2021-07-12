ac1e30c0d6fdd89239f013067b1b794637401edb
JYeonDev <whddus4837@gmail.com>
2021-07-12 12:39:08 +0900

    VISUAL section -fin

--- js/main.js
+++ js/main.js
@@ -36,3 +36,13 @@ window.addEventListener('scroll', _.throttle(function() {
   }
 }, 300));
 // _.throttle(함수, 시간); 함수가 무분별하게 사용하는 걸 방지(loadsh)
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index){
+  // gsap.to(요소, 지속시간, 옵션); 애니메이션 라이브러리(gsap)
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
